a8c945483912d253e398246773cc8ece9c96d36f
FamHaque <fahmidulhaque1997@gmail.com>
2024-08-18 18:32:20 +0100

    fix formatting

--- scripts/download_invoice.py
+++ scripts/download_invoice.py
@@ -5,9 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotenv import load_dotenv
 
-driver_path = "path_to_chromedriver"
-profile_path = "C:/Users/<YourUsername>/AppData/Local/Google/Chrome/User Data/Default"
+load_dotenv()
+driver_path = os.getenv("DRIVER_PATH")
+profile_path = os.getenv("PROFILE_PATH")
 chrome_options = Options()
 chrome_options.add_argument(f"user-data-dir={profile_path}")
 service = Service(driver_path)
@@ -20,9 +23,9 @@ try:
         EC.presence_of_element_located((By.NAME, "username"))
     )
     password_field = driver.find_element(By.NAME, "password")
-    
+
     username_field.send_keys("your_username")
-    password_field.send_keys("your_password") 
+    password_field.send_keys("your_password")
 
     password_field.send_keys(Keys.RETURN)
 
